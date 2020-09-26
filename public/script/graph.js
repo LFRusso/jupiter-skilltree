04c8d48ced7f7ce1540044641d61a85ba60f65f1
Luiz Fernando Santos <lf.santos@usp.br>
2020-09-26 18:15:12 -0300

    Primeiro esboço da árvore

--- public/script/graph.js
+++ public/script/graph.js
@@ -2,17 +2,48 @@ class Graph {
     constructor(nodes, width, height) {   
         let r = (1/5)*(width+height)/2      
         for (let i=0; i<nodes.length; i++) {
-            nodes[i].x = width/2 + r*Math.cos(i*2*Math.PI/nodes.length);
-            nodes[i].y = height/2 + r*Math.sin(i*2*Math.PI/nodes.length);
+            let ang = i*2*Math.PI/nodes.length;
+            nodes[i].x = width/2 + r*Math.cos(ang);
+            nodes[i].y = height/2 + r*Math.sin(ang);
+            nodes[i].ang = ang;
+
+            nodes[i].clicked = false;
         }
         
+        this.r = r
         this.nodes = nodes;
         this.width = width;
         this.height = height;
     }
 
-    add_node(node) {
+    index_by_id(id) {
+        for (let i=0; i<this.nodes.length; i++) {
+            if (id == this.nodes[i].id) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
+    add_node(node) {
+        if (G.nodes.includes(node)) {
+            return;
+        }
+        
+        let ang = 0;
+        for (let i=0; i<node.requisitos.length; i++) {
+            let index = this.index_by_id(node.requisitos[i]);
+            ang += this.nodes[index].ang;
+        }
+        ang = ang/node.requisitos.length + Math.random() * (0.4 + 0.4) - 0.4;
+
+        node.x = this.width/2 + (50*node.requisitos.length + this.r)*Math.cos(ang);
+        node.y = this.height/2 + (50*node.requisitos.length + this.r)*Math.sin(ang);
+        node.ang = ang;
+        node.clicked = false;
+        console.log(node);
+
+        this.nodes.push(node);
     }
 
     add_nodes_from(nodes) {
@@ -24,13 +55,38 @@ class Graph {
     }
 
     draw() {
-        for (let i=0; i<this.nodes.length; i++) {
-            fill(255, 255, 255, 255);
-            circle(this.nodes[i].x , this.nodes[i].y, 20);
-        
-            textSize(12);
-            fill(125, 125, 125, 255);
-            text(this.nodes[i].id, this.nodes[i].x, this.nodes[i].y);
+
+        // Desenhando vértices
+        for (let i=0; i<this.nodes.length; i++) {    
+            stroke(0,0);    
+            if (this.nodes[i].clicked) {
+                fill(255, 255, 255, 255);
+                circle(this.nodes[i].x , this.nodes[i].y, 20);
+
+                textSize(12);
+                fill(255, 0, 0, 255);
+                text(this.nodes[i].id, this.nodes[i].x, this.nodes[i].y);
+            } else {
+                fill(200, 200, 200, 200);
+                circle(this.nodes[i].x , this.nodes[i].y, 20);
+
+
+                textSize(12);
+                fill(125, 125, 125, 255);
+                text(this.nodes[i].id, this.nodes[i].x, this.nodes[i].y);
+            }
           }
+
+        // Desenhando arestas
+        for (let i=0; i<this.nodes.length; i++) {
+            for (let j=0; j<this.nodes[i].requisitos.length; j++) {
+                let index = this.index_by_id(this.nodes[i].requisitos[j])
+                stroke(255);
+                line(this.nodes[i].x, this.nodes[i].y, this.nodes[index].x, this.nodes[index].y);
+            }
+        } 
+    }
+
+    handle_input(x, y) {
     }
 }
--- public/script/sketch.js
+++ public/script/sketch.js
@@ -20,11 +20,48 @@ function setup() {
     }
   }
   G = new Graph(initial_nodes, width, height);
-
   createCanvas(width, height);
+}
+
+
+function update_graph() {
+  for (entry in data) {
+    let prereqs = data[entry]['requisitos'].slice();
+    for (let i=0; i<G.nodes.length; i++) {
+      
+      if (prereqs.includes(G.nodes[i].id) && G.nodes[i].clicked) {
+        let index = prereqs.indexOf(G.nodes[i].id);
+        prereqs.splice(index, 1);
+      }
+    }
+    if (prereqs.length == 0) {
+      G.add_node(data[entry]);
+    }
+  }
 }
   
 function draw() {
-    background(0);
-    G.draw();
+  background(0);
+  G.draw();
+
+  for(let i=0; i<G.nodes.length; i++) {
+    if(dist(mouseX, mouseY, G.nodes[i].x, G.nodes[i].y) < 5) {
+      fill(125, 10, 125, 255);
+      circle(G.nodes[i].x , G.nodes[i].y, 20);
+
+      textSize(30);
+      fill(255, 255, 255, 255);
+      text(G.nodes[i].nome, G.width/2 - G.r/2, G.height/2);
+    }
+  }
+
+  update_graph();
+}
+
+function mouseClicked() {
+  for(let i=0; i<G.nodes.length; i++) {
+    if(dist(mouseX, mouseY, G.nodes[i].x, G.nodes[i].y) < 5) {
+      G.nodes[i].clicked = true;
+    }
+  }
 }
